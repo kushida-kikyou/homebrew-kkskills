e6aebe42addfc13fff294b85725d7b0314e0fda8
栉田桔梗 <git_kushida@163.com>
2026-04-24 15:14:16 +0800

    Update kkskills.rb

--- Formula/kkskills.rb
+++ Formula/kkskills.rb
@@ -2,16 +2,14 @@ class Kkskills < Formula
   desc "CLI tool to manage and link AI agent skills"
   homepage "https://github.com/kushida-kikyou/kkskills"
   url "https://github.com/kushida-kikyou/kkskills/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "f0878d62265724ceff95d10a1852be2f029be11f8f20ad893c7075e4b52580cf  kkskills.tar.gz"
+  sha256 "f0878d62265724ceff95d10a1852be2f029be11f8f20ad893c7075e4b52580cf"
   license "MIT"
 
   def install
-    bin.install "bin/kkskills"
+    # 安装全部文件到 libexec
+    libexec.install Dir["*"]
 
-    # 安装 lib 目录（关键）
-    libexec.install Dir["lib"]
-
-    # 让脚本能找到 lib
+    # 创建可执行入口
     (bin/"kkskills").write <<~EOS
       #!/bin/bash
       exec "#{libexec}/bin/kkskills" "$@"
